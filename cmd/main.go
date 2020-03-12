9aabd9c29e13a014389a24d71adacde7ab1b9cd1
Jordan Ocokoljic <jordan@libreware.org>
2020-03-12 12:34:26 +1100

    Added help action

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,7 @@ func main() {
 	case "init":
 	case "build":
 	case "stub":
-	case "":
+	case "help", "":
 		usage()
 	default:
 		fmt.Printf("option: %s not recognised\n", action)
@@ -39,6 +39,8 @@ build	Scans the source for calls to the library functions, and reads the
 stub	Scans the source for calls to the library functions, and generates
 	functions that read the contents of those files as they exist on
 	the file system for testing and development.
+
+help	Shows this manual.
 `))
 
 	os.Exit(0)
