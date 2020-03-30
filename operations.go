33796f750656dfe2ee4e71e9d56f5da2f3eb9cdc
Jordan Ocokoljic <jordan@libreware.org>
2020-03-30 21:27:02 +1100

    Refactoring, didn't like the design

--- operations.go
+++ operations.go
@@ -1,33 +1,8 @@
 package zap
 
-import (
-	"errors"
-	"fmt"
-	"os"
-	"path"
-	"zap/zapped"
-)
-
-var (
-	// ErrorInitNoOp is returned when OperationInit is called in a directory
-	// where the zap library has already been initialized.
-	ErrorInitNoOp = errors.New("zap library already initialized")
-)
-
 // OperationInit creates a new folder containg the zap library for use within
 // projects that use the tool.
 func OperationInit(wd string) error {
-	if checkDirectoryExists(path.Join(wd, "zapped")) {
-		return ErrorInitNoOp
-	}
-
-	err := os.Mkdir("zapped", os.ModePerm)
-	if err != nil {
-		return err
-	}
-
-	zapped.OpenBox("zapped")
-
 	return nil
 }
 
@@ -36,29 +11,6 @@ func OperationInit(wd string) error {
 // within them and makes them accessible to the library created so the project
 // can be built.
 func OperationBuild(wd string) error {
-	skip := []string{
-		".git",
-		"testdata",
-		"zapped",
-	}
-
-	packages, err := getPackagesInProject(wd, skip)
-	if err != nil {
-		return err
-	}
-
-	var boxes []string
-	for _, pkg := range packages {
-		found, err := getBoxesInPackage(pkg)
-		if err != nil {
-			return err
-		}
-
-		boxes = append(boxes, found...)
-	}
-
-	fmt.Printf("%v\n", boxes)
-
 	return nil
 }
 
