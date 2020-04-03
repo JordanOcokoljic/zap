e8e8211aadc8acee77f412c97d06da5377bb4c9d
Jordan Ocokoljic <jordan@libreware.org>
2020-04-03 14:11:59 +1100

    Wrote function to get all packages in project

--- zap.go
+++ zap.go
@@ -1,1 +1,38 @@
 package zap
+
+import (
+	"go/build"
+	"os"
+	"path/filepath"
+)
+
+// GetPackagesInProject will return the package in the current directory, as
+// well as all the subdirectories under it. Under the hood it uses Walk, so
+// it won't follow symbolic links.
+func GetPackagesInProject(wd string, skip []string) ([]*build.Package, error) {
+	var packages []*build.Package
+
+	fn := func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
+
+		for _, name := range skip {
+			if info.Name() == name {
+				return filepath.SkipDir
+			}
+		}
+
+		pkg, err := build.ImportDir(path, 0)
+		if err != nil {
+			return err
+		}
+
+		packages = append(packages, pkg)
+
+		return nil
+	}
+
+	err := filepath.Walk(wd, fn)
+	return packages, err
+}
--- zap_test.go
+++ zap_test.go
@@ -1,1 +1,28 @@
-package zap
+package zap_test
+
+import (
+	"reflect"
+	"testing"
+	"zap"
+)
+
+func AssertStringSliceMatch(t *testing.T, expected []string, actual []string) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+}
+
+func TestGetPackagesInProject(t *testing.T) {
+	pkgs, err := zap.GetPackagesInProject(".", []string{".git"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	var names []string
+	for _, pkg := range pkgs {
+		names = append(names, pkg.Name)
+	}
+
+	AssertStringSliceMatch(t, []string{"zap", "main", "zapped"}, names)
+}
