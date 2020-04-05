d0befb3b09fca0812b8ab768447275ae8ec711d5
Jordan Ocokoljic <jordan@libreware.org>
2020-04-05 17:19:24 +1000

    Implemented parser and tests

--- zap.go
+++ zap.go
@@ -1,17 +1,55 @@
 package zap
 
 import (
+	"fmt"
 	"go/ast"
 	"go/build"
+	"go/parser"
+	"go/token"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+// Resource is used to track each unique Key passed to a call to Resource() and
+// the path specified in the call.
+type Resource struct {
+	Key  string
+	Path string
+}
+
+// aggregateError is a collection of errors that fullfils the error interface,
+// so it can be passed up, while still containing all the information.
+type aggregateError struct {
+	errors []error
+}
+
+// Add adds a new error to the list of errors.
+func (ag *aggregateError) Add(err error) {
+	ag.errors = append(ag.errors, err)
+}
+
+// Returns the value of the aggregateError, which is just a big collection of
+// all the other errors. Fulfils the Error interface.
+func (ag aggregateError) Error() string {
+	var str string
+
+	for i, err := range ag.errors {
+		if i == 0 {
+			str = err.Error()
+			continue
+		}
+
+		str = fmt.Sprintf("%s\n%s", str, err.Error())
+	}
+
+	return str
+}
+
 // GetPackagesInProject will return the package in the current directory, as
 // well as all the subdirectories under it. Under the hood it uses Walk, so
 // it won't follow symbolic links.
-func GetPackagesInProject(wd string, skip []string) ([]*build.Package, error) {
+func GetPackagesInProject(wd string) ([]*build.Package, error) {
 	var packages []*build.Package
 
 	fn := func(path string, info os.FileInfo, err error) error {
@@ -19,15 +57,14 @@ func GetPackagesInProject(wd string, skip []string) ([]*build.Package, error) {
 			return nil
 		}
 
-		for _, name := range skip {
-			if info.Name() == name {
-				return filepath.SkipDir
-			}
-		}
-
 		pkg, err := build.ImportDir(path, 0)
+
 		if err != nil {
-			return err
+			if !strings.HasPrefix(err.Error(), "no buildable Go source") {
+				return err
+			}
+
+			return filepath.SkipDir
 		}
 
 		packages = append(packages, pkg)
@@ -57,4 +94,158 @@ func getZappedImportName(file *ast.File) string {
 	}
 
 	return name
+}
+
+// Track the types of errors that could occur so that generateParseError knows
+// what message to use.
+const (
+	errorUnknownCall uint8 = iota
+	errorBadType
+)
+
+// generateParseError will return an error with correct formatting describing
+// what was incorrect about the scanned source.
+func generateParseError(fset *token.FileSet, p token.Pos, err uint8) error {
+	pos := fset.Position(p)
+	filename := pos.Filename
+
+	// Reduces unecessary duplication of code.
+	format := func(msg string) string {
+		return fmt.Sprintf("%s:%d:%d: %s", filename, pos.Line, pos.Column, msg)
+	}
+
+	var msg string
+	switch err {
+	case errorUnknownCall:
+		msg = format("expected Resource() but was something else")
+	case errorBadType:
+		msg = format("calls to Resource() require string literals")
+	}
+
+	return fmt.Errorf(msg)
+}
+
+// parse will walk the AST and identify calls to Resource() and extract
+// the key and the path from them. It will return an error
+func parse(f *ast.File, fset *token.FileSet, imp string) ([]Resource, error) {
+	var resources []Resource
+	var errors aggregateError
+
+	// State based parsing let's us solve this problem without needing to have
+	// a million different variables tracking everything.
+	var state uint8
+	const (
+		NilState uint8 = iota
+		ExpectingResourceCall
+		ExpectingKey
+		ExpectingPath
+	)
+
+	// Error handling code being pulled out into it's own closure reduces
+	// repetition of simple code.
+	handleError := func(node ast.Node, errorType uint8) {
+		err := generateParseError(fset, node.Pos(), errorType)
+		errors.Add(err)
+	}
+
+	// Pulling the code out of the switch improves the readability, and ensures
+	// clarity of logic.
+	resolveIdent := func(node *ast.Ident) uint8 {
+		switch state {
+		case NilState:
+			if node.Name == imp {
+				return ExpectingResourceCall
+			}
+
+		case ExpectingResourceCall:
+			if node.Name != "Resource" {
+				handleError(node, errorUnknownCall)
+				return NilState
+			}
+
+			return ExpectingKey
+
+		case ExpectingKey, ExpectingPath:
+			handleError(node, errorBadType)
+			return NilState
+		}
+
+		return state
+	}
+
+	resolveBasicLit := func(node *ast.BasicLit) uint8 {
+		switch state {
+		case ExpectingKey:
+			if node.Kind != token.STRING {
+				handleError(node, errorBadType)
+				return NilState
+			}
+
+			res := Resource{Key: node.Value[1 : len(node.Value)-1]}
+			resources = append(resources, res)
+			return ExpectingPath
+
+		case ExpectingPath:
+			if node.Kind != token.STRING {
+				handleError(node, errorBadType)
+				return NilState
+			}
+
+			res := resources[len(resources)-1]
+			res.Path = node.Value
+			return NilState
+		}
+
+		return state
+	}
+
+	// Walk the AST.
+	ast.Inspect(f, func(node ast.Node) bool {
+		if node == nil {
+			return false
+		}
+
+		switch n := node.(type) {
+		case *ast.Ident:
+			state = resolveIdent(n)
+
+		case *ast.BasicLit:
+			state = resolveBasicLit(n)
+
+		default:
+			if state == ExpectingKey || state == ExpectingPath {
+				handleError(node, errorBadType)
+			}
+
+			state = NilState
+		}
+
+		return true
+	})
+
+	if errors.Error() != "" {
+		return resources, errors
+	}
+
+	return resources, nil
+}
+
+// GetResourcesInFile will parse through a file, and identify all calls to
+// Resource(), it will return a slice of Boxes so that Zap and pack these into
+// Go source.
+func GetResourcesInFile(fpath string) ([]Resource, error) {
+	var resources []Resource
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, fpath, nil, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	importName := getZappedImportName(f)
+	if importName == "" || importName == "_" || importName == "." {
+		return resources, nil
+	}
+
+	return parse(f, fset, importName)
 }
--- zap_test.go
+++ zap_test.go
@@ -1,10 +1,12 @@
 package zap
 
 import (
+	"errors"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -26,7 +28,7 @@ func parseGo(t *testing.T, src string) (*ast.File, *token.FileSet) {
 	t.Helper()
 
 	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, "", src, 0)
+	file, err := parser.ParseFile(fset, "main.go", src, 0)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -34,8 +36,27 @@ func parseGo(t *testing.T, src string) (*ast.File, *token.FileSet) {
 	return file, fset
 }
 
+func assertResourceSliceMatch(t *testing.T, exp []Resource, act []Resource) {
+	t.Helper()
+
+	if len(exp) != len(act) {
+		t.Error("Slices did not match")
+		return
+	}
+
+	for i := range exp {
+		expected := exp[i]
+		actual := exp[i]
+
+		if expected.Key != actual.Key || expected.Path != actual.Path {
+			t.Error("Slices did not match")
+			return
+		}
+	}
+}
+
 func TestGetPackagesInProject(t *testing.T) {
-	pkgs, err := GetPackagesInProject(".", []string{".git"})
+	pkgs, err := GetPackagesInProject(".")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
@@ -64,6 +85,134 @@ func TestGetZappedImportName(t *testing.T) {
 			file, _ := parseGo(s, test.code)
 			name := getZappedImportName(file)
 			assertString(s, test.expected, name)
+		})
+	}
+}
+
+func TestAggregateError(t *testing.T) {
+	var ag aggregateError
+
+	assertString(t, "", ag.Error())
+
+	ag.Add(errors.New("Test 1"))
+	assertString(t, "Test 1", ag.Error())
+
+	ag.Add(errors.New("Test 2"))
+	assertString(t, "Test 1\nTest 2", ag.Error())
+}
+
+func TestGenerateParserError(t *testing.T) {
+	code := "package main"
+
+	f, fset := parseGo(t, code)
+
+	err := generateParseError(fset, f.Pos(), errorUnknownCall)
+	expected := "main.go:1:1: expected Resource() but was something else"
+	assertString(t, expected, err.Error())
+
+	err = generateParseError(fset, f.Pos(), errorBadType)
+	expected = "main.go:1:1: calls to Resource() require string literals"
+	assertString(t, expected, err.Error())
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name              string
+		err               string
+		expectedResources []Resource
+		code              string
+	}{
+		{
+			name: "GoodSample",
+			err:  "",
+			expectedResources: []Resource{
+				{Key: "A", Path: "scripts/"},
+				{Key: "B", Path: "sql/"},
+			},
+			code: `
+package test
+
+import "zapped"
+
+func main() {
+	zapped.Resource("A", "scripts/")
+	zapped.Resource("B", "sql/")
+}`,
+		},
+		{
+			name: "WithRenamedImport",
+			err:  "",
+			expectedResources: []Resource{
+				{Key: "A", Path: "scripts/"},
+				{Key: "B", Path: "sql/"},
+			},
+			code: `
+package test
+
+import z "zapped"
+
+func main() {
+	z.Resource("A", "scripts/")
+	z.Resource("B", "sql/")
+}`,
+		},
+		{
+			name: "WithIdentKeyInsteadOfLiteral",
+			err:  "main.go:8:18: calls to Resource() require string literals",
+			expectedResources: []Resource{
+				{Key: "B", Path: "sql/"},
+			},
+			code: `
+package test
+
+import "zapped"
+
+func main() {
+	key := "A"
+
+	zapped.Resource(key, "scripts/")
+	zapped.Resource("B", "sql/")
+}`,
+		},
+		{
+			name: "WithIdentPathInsteadOfLiteral",
+			err:  "main.go:8:23: calls to Resource() require string literals",
+			expectedResources: []Resource{
+				{Key: "A"},
+				{Key: "B", Path: "sql/"},
+			},
+			code: `
+package test
+
+import "zapped"
+
+func main() {
+	key := "scripts/"
+
+	zapped.Resource("A", key)
+	zapped.Resource("B", "sql/")
+}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(s *testing.T) {
+			src := strings.TrimSpace(test.code)
+			f, fset := parseGo(s, src)
+
+			imp := getZappedImportName(f)
+			resources, err := parse(f, fset, imp)
+
+			if test.err == "" && err != nil {
+				msg := "an error occured and isn't expected\n%s"
+				s.Errorf(msg, err.Error())
+			}
+
+			if test.err != "" {
+				assertString(s, test.err, err.Error())
+			}
+
+			assertResourceSliceMatch(s, test.expectedResources, resources)
 		})
 	}
 }
